c6c6a8e050a3a34ce80dc03854478446d03459f2
Sharan <sharan@mallow-tech.com>
2022-01-12 23:10:18 +0530

    - Added Composer install scripts.

--- composer-install.sh
+++ composer-install.sh
@@ -6,17 +6,8 @@ mkdir /usr/local/dockr/composer
 # Download and store composer installer locally
 curl -sS https://getcomposer.org/installer >> /usr/local/dockr/composer/installer
 
-# Install version 1.6.0
-php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_1.6.0 --version=1.6.0
-
-# Install version 1.8.0
-php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_1.8.0 --version=1.8.0
-
-# Install version 1.10.0
-php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_1.10.0 --version=1.10.0
-
-# Install version 2.0.0
-php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.0.0 --version=2.0.0
+# Install version 1.8.6
+php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_1.8.6 --version=1.8.6
 
 # Install version 2.1.0
 php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.1.0 --version=2.1.0
