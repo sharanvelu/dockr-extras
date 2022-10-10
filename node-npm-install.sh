d01b6c1f14c3fa9e6bcdd553c73c22596409479e
Sharan <sharan@mallow-tech.com>
2022-10-10 11:09:00 +0530

    - Added NPM install script

--- node-npm-install.sh
+++ node-npm-install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install "${VERSION:-16.13.0}" | bash
+curl -o- curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+VERSION=${VERSION:-18.10.0}
+
+nvm install "${VERSION}" | bash
+#nvm install node
+npm install -g npm@latest
+
+ln -s /root/.nvm/versions/node/v${VERSION}/bin/node /usr/local/bin/node
+ln -s /root/.nvm/versions/node/v${VERSION}/bin/npm /usr/local/bin/npm
+
