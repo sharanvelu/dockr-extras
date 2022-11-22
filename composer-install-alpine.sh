c693f072f97634db2b483e815c5a482773a44da2
Sharan <sharan@mallow-tech.com>
2022-11-22 13:12:53 +0530

    - Added composer install command for alpine

---
+++ composer-install-alpine.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Create composer version directory
+mkdir /usr/local/dockr/composer
+
+# Download and store composer installer locally
+curl -sS https://getcomposer.org/installer >> /usr/local/dockr/composer/installer
+
+# Install version 1.8.6
+php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_1.8.6 --version=1.8.6
+
+# Install version 2.1.0
+php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.1.0 --version=2.1.0
+
+# Install version 2.2.0
+php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.2.0 --version=2.2.0
+
+# Set 2.2.0 as default version
+cp /usr/local/dockr/composer/composer_2.2.0 /usr/bin/composer
