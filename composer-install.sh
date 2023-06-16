dbe59fa994357a6f3a831096bf46fee6608bfb9e
Sharan <sharan@mallow-tech.com>
2023-06-16 10:49:46 +0530

    - Updated Composer versions

--- composer-install.sh
+++ composer-install.sh
@@ -12,8 +12,8 @@ php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer 
 # Install version 2.1.0
 php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.1.0 --version=2.1.0
 
-# Install version 2.2.0
-php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.2.0 --version=2.2.0
+# Install version 2.5.0
+php /usr/local/dockr/composer/installer --install-dir=/usr/local/dockr/composer --filename=composer_2.5.0 --version=2.5.0
 
 # Set 2.2.0 as default version
-cp /usr/local/dockr/composer/composer_2.2.0 /usr/bin/composer
+cp /usr/local/dockr/composer/composer_2.5.0 /usr/bin/composer
